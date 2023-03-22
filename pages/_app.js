3f2fca085ab7022c3e704cb34cae91cc1c9084a9
Егор Ткаченко <egortkachenco@gmail.com>
2023-03-22 18:19:37 +0200

    disable auto zoom

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import AuthContext from '@/utils/authContext'
 import Modals from '@/components/modals'
 import { observer } from 'mobx-react-lite'
 import AuthStore from '@/store/AuthStore'
+import Head from 'next/head'
 
 const App = observer(({ Component, pageProps, router }) => {
   const [modal, setModal] = useState(null)
@@ -24,15 +25,23 @@ const App = observer(({ Component, pageProps, router }) => {
   }, [user, router.asPath])
 
   return (
-    <ThemeProvider theme={theme}>
-      <AuthContext.Provider value={{ isLogged, setIsLogged }}>
-        <ModalContext.Provider value={{ modal, openModal: setModal }}>
-          <GlobalStyles />
-          <Component {...pageProps} />
-          <Modals />
-        </ModalContext.Provider>
-      </AuthContext.Provider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <AuthContext.Provider value={{ isLogged, setIsLogged }}>
+          <ModalContext.Provider value={{ modal, openModal: setModal }}>
+            <GlobalStyles />
+            <Component {...pageProps} />
+            <Modals />
+          </ModalContext.Provider>
+        </AuthContext.Provider>
+      </ThemeProvider>
+    </>
   )
 })
 
