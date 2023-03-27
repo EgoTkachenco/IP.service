bf4133fc5dee706543bc5b85823a9da9c6ad1143
Егор Ткаченко <egortkachenco@gmail.com>
2023-03-27 16:15:57 +0300

    add rewrites for backend services

--- next.config.js
+++ next.config.js
@@ -6,6 +6,62 @@ const nextConfig = {
   },
   rewrites: async () => {
     return [
+      // Main service
+      {
+        source: '/:ip',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:ip?token=:token`,
+      },
+      // ASN service
+      {
+        source: '/asn/:ip',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/asn/:ip?token=:token`,
+      },
+      // Domains service
+      {
+        source: '/domains/:ip',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/domains/:ip?token=:token`,
+      },
+      // Ranges service
+      {
+        source: '/ranges/:ip',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/ranges/:ip?token=:token`,
+      },
+      // Whois service
+      {
+        source: '/whois/:ip',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/whois/:ip?token=:token`,
+      },
+      // flags
       {
         source: '/flags/:id',
         destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/public/flag/:id`,
