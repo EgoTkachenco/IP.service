466fde51101eccf8145904cb7fa545bd0ee76a18
Егор Ткаченко <egortkachenco@gmail.com>
2023-04-18 11:33:43 +0300

    update custom plan selected services logic

--- components/landing/blocks/CustomPlan.jsx
+++ components/landing/blocks/CustomPlan.jsx
@@ -6,25 +6,66 @@ import CustomServiceCard from '@/components/reusable/CustomServiceCard'
 
 const CustomPlan = ({ period, details = [], onSubscribe }) => {
   const [selected, setSelected] = useState([])
+  const basic_support_id = details?.find(
+    (el) => el.name === 'Basic support'
+  )?.id
+  const priority_support_id = details?.find(
+    (el) => el.name === 'Priority Support'
+  )?.id
+
   const handleSelect = (value) => {
-    if (selected.includes(value))
-      setSelected(selected.filter((v) => v !== value))
-    else setSelected([...selected, value])
+    let newSelected = []
+    if (selected.includes(value)) {
+      newSelected = selected.filter((v) => v !== value)
+    } else {
+      newSelected = [...selected, value]
+    }
+
+    // add basic support
+    if (
+      !newSelected.includes(basic_support_id) &&
+      !newSelected.includes(priority_support_id) &&
+      newSelected.length >= 1
+    )
+      newSelected.push(basic_support_id)
+
+    // remove basic if priority support added or no services selected
+    if (
+      value === priority_support_id ||
+      (newSelected.length === 1 && newSelected.includes(basic_support_id))
+    )
+      newSelected = newSelected.filter((v) => v !== basic_support_id)
+
+    setSelected(newSelected)
   }
-  const removeItem = (id) => setSelected(selected.filter((v) => v !== id))
+  const removeItem = (id) => {
+    if (id === basic_support_id) return
+
+    let newSelected = selected.filter((v) => v !== id)
+
+    if (id === priority_support_id && newSelected.length >= 1)
+      newSelected.push(basic_support_id)
+
+    if (newSelected.length === 1 && newSelected.includes(basic_support_id))
+      newSelected = []
+
+    setSelected(newSelected)
+  }
   return (
     <Container>
       <CardsContainer>
         {details &&
-          details.map((detail) => (
-            <CustomServiceCard
-              key={detail.id}
-              period={period}
-              details={detail}
-              isActive={selected.includes(detail.id)}
-              handleSelect={() => handleSelect(detail.id)}
-            />
-          ))}
+          details
+            .filter((el) => el.id !== basic_support_id)
+            .map((detail) => (
+              <CustomServiceCard
+                key={detail.id}
+                period={period}
+                details={detail}
+                isActive={selected.includes(detail.id)}
+                handleSelect={() => handleSelect(detail.id)}
+              />
+            ))}
       </CardsContainer>
       <SelectedServicesCart
         period={period}
