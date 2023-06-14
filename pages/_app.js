71cef5fc6f2f8616c20a20c4714122b5bc2f857c
Егор Ткаченко <egortkachenco@gmail.com>
2023-06-14 15:53:37 +0300

    fix gtag load

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import AuthContext from '@/utils/authContext'
 import Modals from '@/components/modals'
 import { observer } from 'mobx-react-lite'
 import AuthStore from '@/store/AuthStore'
+import Script from 'next/script'
 
 const App = observer(({ Component, pageProps, router }) => {
   const [modal, setModal] = useState(null)
@@ -30,15 +31,49 @@ const App = observer(({ Component, pageProps, router }) => {
   }, [router.query])
 
   return (
-    <ThemeProvider theme={theme}>
-      <AuthContext.Provider value={{ isLogged, setIsLogged }}>
-        <ModalContext.Provider value={{ modal, openModal: setModal }}>
-          <GlobalStyles />
-          <Component {...pageProps} />
-          <Modals />
-        </ModalContext.Provider>
-      </AuthContext.Provider>
-    </ThemeProvider>
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QJRZWNMN14"
+        strategy="beforeInteractive"
+      ></Script>
+      <Script strategy="beforeInteractive">
+        {`
+						window.dataLayer = window.dataLayer || []; 
+						function gtag(){window.dataLayer.push(arguments)}
+            gtag('js', new Date()); 
+						gtag('config', 'G-QJRZWNMN14');
+						`}
+      </Script>
+      {/* <!-- Google Tag Manager --> */}
+      <Script strategy="beforeInteractive">{`
+					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-5V7BD43');
+					`}</Script>
+      {/* <!-- End Google Tag Manager --> */}
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5V7BD43"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
+      <ThemeProvider theme={theme}>
+        <AuthContext.Provider value={{ isLogged, setIsLogged }}>
+          <ModalContext.Provider value={{ modal, openModal: setModal }}>
+            <GlobalStyles />
+            <Component {...pageProps} />
+            <Modals />
+          </ModalContext.Provider>
+        </AuthContext.Provider>
+      </ThemeProvider>
+    </>
   )
 })
 
--- pages/_document.js
+++ pages/_document.js
@@ -57,38 +57,6 @@ export default class MyDocument extends Document {
               "window.addEventListener('resize', () => {let vh = window.innerHeight * 0.01;document.documentElement.style.setProperty('--vh', `${vh}px`);});"
             }
           </Script>
-          {/* <!-- Google tag (gtag.js) --> */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-QJRZWNMN14"
-            strategy="beforeInteractive"
-          ></Script>
-          <Script strategy="beforeInteractive">
-            {`
-						window.dataLayer = window.dataLayer || []; 
-						function gtag(){window.dataLayer.push(arguments)}
-            gtag('js', new Date()); 
-						gtag('config', 'G-QJRZWNMN14');
-						`}
-          </Script>
-          {/* <!-- Google Tag Manager --> */}
-          <Script strategy="beforeInteractive">{`
-					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-5V7BD43');
-					`}</Script>
-          {/* <!-- End Google Tag Manager --> */}
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5V7BD43"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
         </Head>
         <body>
           <Main />
